19eadc3ce5b575b0e708c75dbdd7a437a0a8ef91
Erik Deijl <erik.deijl@gmail.com>
2015-01-02 14:08:02 +0100

    Use Bootstrap to impove layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,14 +1,15 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 import unittest
 
-class NewVisitorTest(LiveServerTestCase):    
+class NewVisitorTest(StaticLiveServerTestCase):    
     def setUp(self):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
+        self.browser.refresh()
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
@@ -97,5 +98,5 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta = 5
+            delta = 10
         )
